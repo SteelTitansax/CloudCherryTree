ad114ae3c6d85ab6d08c0d7f11a60e076fb062fa
SteelTitansax <manuel.portero.leiva@gmail.com>
2024-02-16 02:20:17 +0100

    Final Commit

--- App.py
+++ App.py
@@ -1,70 +1,119 @@
 from flask import Flask,url_for,flash,render_template,request,redirect
+import pyodbc
+
 
 app = Flask(__name__)
-'''app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'SecretoAcero11!'
-app.config['MYSQL_DB'] = 'cloudFlaskCherryTreeDB'
-mysql = MySQL(app)
-'''
+# Session and cache
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'super secret key'
+
+
+server = 'beerprocesssimulatorsql.database.windows.net'
+database = 'CloudCherryTreeDB'
+username = 'titansax'
+password = 'SecretoGlasgow11!'
+drivers = [item for item in pyodbc.drivers()]
+driver = drivers[-1]
+
+
 
 @app.route('/')
 
 def Index():
-    '''cur = mysql.connection.cursor()
-    mysql.connection.commit()
+    #Create a connection string
+    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
 
-    cur.execute('SELECT * FROM Results')
+    cur = cnxn.cursor()
+
+    cur.execute('SELECT * FROM posts')
     data = cur.fetchall()
-    '''
-    return render_template('index.html')
-'''
-@app.route('/add_contact', methods=['POST'])
-def add_contact():
+    cur.execute('SELECT DISTINCT technology FROM [dbo].[posts]')
+    uniquedata = cur.fetchall()
+    return render_template('index.html',posts=data, technologies= uniquedata)
+
+@app.route('/filter/<technology>')
+
+def filter_technology(technology):
+    technology_selected = technology
+
+    #Create a connection string
+    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cur = cnxn.cursor()
+    cur.execute('SELECT * FROM [dbo].[posts] Where Technology = ?',technology_selected)
+    data = cur.fetchall()
+    cur.execute('SELECT DISTINCT technology FROM [dbo].[posts]')
+    uniquedata = cur.fetchall()
+    return render_template('index.html',posts=data, technologies= uniquedata)
+
+
+@app.route('/add/<id>')
+def create_post_form(id):
+    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cur = cnxn.cursor()
+
+    cur.execute('SELECT * FROM posts WHERE id = ?', id)
+    data = cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('add-post.html', post = data[0])
+
+
+@app.route('/add_post', methods=['POST'])
+def add_post():
     if request.method == 'POST':
        technology = request.form['technology']
        post = request.form['post']
-       cur = mysql.connection.cursor()
-       cur.execute('INSERT INTO posts (technology,post) VALUES(%s,%s)',
+
+       # Create a connection string
+       cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+       cur = cnxn.cursor()
+       cur.execute('INSERT INTO posts (technology,post) VALUES(?,?)',
        (technology,post))
-       mysql.connection.commit()
+       cnxn.commit()
+       cnxn.close()
        flash('Post added successfully')
        return redirect(url_for('Index'))
 
 @app.route('/edit/<id>')
-def edit_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM contacts WHERE id = %s', (id))
+def edit_post(id):
+    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cur = cnxn.cursor()
+
+    cur.execute('SELECT * FROM posts WHERE id = ?', id)
     data = cur.fetchall()
     cur.close()
     print(data[0])
-    return render_template('edit-contact.html', contact = data[0])
+    return render_template('edit-post.html', post = data[0])
 
 @app.route('/update/<id>', methods=['POST'])
-def update_contact(id):
+def update_post(id):
     if request.method == 'POST':
         technology = request.form['technology']
         post = request.form['post']
-        cur = mysql.connection.cursor()
+        cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+        cur = cnxn.cursor()
         cur.execute("""
-            UPDATE contacts 
+            UPDATE posts 
             SET fullname = %s,
                 technology = %s,
                 post = %s
             WHERE id = %s
         """, (technology,post,id))
-        mysql.connection.commit()
+        cnxn.commit()
+        cnxn.close()
         flash('Post Updated Succesfully')
         return redirect(url_for('Index'))
 
 @app.route('/delete/<string:id>')
-def delete_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
-    mysql.connection.commit()
+def delete_post(id):
+    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cur = cnxn.cursor()
+    cur.execute('DELETE FROM posts WHERE id = {0}'.format(id))
+    cnxn.commit()
+    cnxn.close()
     flash('Post deleted successfully')
     return redirect(url_for('Index'))
-'''
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
